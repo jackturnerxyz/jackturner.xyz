9a76ca46a1519b2c80b4b2e80162baae82c17815
Jack B. Turner <archjacksont@gmail.com>
2025-10-05 15:30:14 -0400

    ZSH stuff fix

--- static/jdds.sh
+++ static/jdds.sh
@@ -32,6 +32,7 @@ chmod +x install.sh
 CHSH='no' RUNZSH='no' KEEP_ZSHRC='yes' ./install.sh
 
 cd
+export ZSH_CUSTOM="~/.oh_my_zsh"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
