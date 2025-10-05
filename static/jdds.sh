7fb3000e72fecff801712c4a1b1066b8bd8c9458
Jack B. Turner <archjacksont@gmail.com>
2025-10-05 15:38:36 -0400

    Typos

--- static/jdds.sh
+++ static/jdds.sh
@@ -32,7 +32,7 @@ chmod +x install.sh
 CHSH='no' RUNZSH='no' KEEP_ZSHRC='yes' ./install.sh
 
 cd
-export ZSH_CUSTOM="~/.oh_my_zsh"
+export ZSH_CUSTOM="~/.oh-my-zsh"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
